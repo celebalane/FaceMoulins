18b87313cf2aee5da4be7f960cfeaabf99e9b2e2
celebalane <celebalane@gmail.com>
2017-02-23 21:35:55 +0100

    amélioration affichage footer en mobile

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ function affichage(id){
     div.style.display = "none";
   }
 }
+
+function affichageFooter(id){
+	var div = document.getElementById(id);
+	if (window.matchMedia("(max-width: 640px)").matches) {
+  		if(div.style.display == "none") {
+   			div.style.display = "block";
+  		}else {
+    		div.style.display = "none";
+  		} 
+	}
+}
