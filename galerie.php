06f4f73312743331f8dba030e58c45aea6db64d3
vincent <vincent-du03@hotmail.fr>
2017-03-10 13:55:48 +0100

    caroussel gallerie ajoutée

--- galerie.php
+++ galerie.php
@@ -4,62 +4,115 @@
 <h1 class="titreSection col-md-12 col-xs-12">Galerie</h1>
 <section class="col-xs-12 col-md-12" id="galerie">
     <h3><strong>Photos</strong></h3>
-    <div class="row">
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie1.jpg" rel="lightbox" data-caption="Inauguration de la 1ère fabrique du numérique en Auvergne en présence de représentants de la mairie, du département, de la région, de l'ESPE, d'organismes administratifs et d'associations, d'adhérents et d'ambassadeurs de FACE Territoire Bourbonnais. "><img src="img/galerie/miniature1.jpg"  alt="inauguration fabrique du numérique" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie2.jpg" rel="lightbox" data-caption="Réunion de travail"><img src="img/galerie/miniature2.jpg" alt="réunion de travail" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie3.jpg" rel="lightbox" data-caption="Participation de FACE à la semaine de l'emploi et de la formation"><img src="img/galerie/miniature3.jpg" alt="FACE à la semaine de l'emploi" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie4.jpg" rel="lightbox" data-caption="Deux membres de FACE : Christine BERGER et Xavier de Soultrait"><img src="img/galerie/miniature4.jpg" alt="Christine Berger et Xavier de Soultrait" /></a>
-        </div>
+     <div class="col-md-12 col-xs-12 col-sm-12"  id="pagePhoto">
+    <div id="my_carousel" class="carousel slide" data-ride="carousel">
+ <!-- Slides -->
+        <div class="carousel-inner">
+            <!-- Page 1 -->
+            <div class="item active">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie1.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie1.jpg" class="imageCarousel" /></a>
+                   
+                </div>
+            </div>               
+        
+         <!-- Page 2 -->
+            <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie2.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie2.jpg" class="imageCarousel" /></a>
+                   
+                </div>
+            </div>
+         <!-- Page 3 -->
+             <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie3.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie3.jpg" class="imageCarousel" /></a>
+                   
+                </div>
+            </div> 
+             <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie4.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie4.jpg" class="imageCarousel" /></a>
+                   
+                </div>
+            </div>
+              <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie5.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie5.jpg" class="imageCarousel" /></a>
+                   
+                </div>
+              </div>
+              <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie6.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie6.jpg" class="imageCarousel" /></a>
+                   
+                </div>
+              </div>
+              <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie7.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie7.jpg" class="imageCarousel" /></a>
+              </div>    
+                </div>
+              <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie8.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie8.jpg" class="imageCarousel" /></a>
+              </div>     
+                </div>
+              <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie9.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie9.jpg" class="imageCarousel" /></a>
+              </div>
+                </div>
+              <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie10.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie10.jpg" class="imageCarousel" /></a>
+              </div>    
+                </div>
+                <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie12.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie12.jpg" class="imageCarousel" /></a>
+               </div>    
+                </div>
+                <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie14.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie14.jpg" class="imageCarousel" /></a>
+               </div>    
+                </div>
+                <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie16.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie16.jpg" class="imageCarousel" /></a>
+               </div>    
+                </div>
+                <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie17.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie17.jpg" class="imageCarousel" /></a>
+               </div>    
+                </div>
+                <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie18.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie18.jpg" class="imageCarousel" /></a>
+               </div>    
+                </div>
+                <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie19.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie19.jpg" class="imageCarousel" /></a>
+               </div>    
+                </div>
+                <div class="item ">  
+                <div class="carousel-page">
+                    <a href="img/galerie/galerie20.jpg" rel="lightbox" class="img-responsive"><img src="img/galerie/galerie20.jpg" class="imageCarousel" /></a>
+               </div>    
+                </div>
+        </div>    
+        <!-- Contrôles -->
+        <a class="left carousel-control" href="#my_carousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#my_carousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
     </div>
-    <div class="row">
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie5.jpg" rel="lightbox" data-caption="Notre ambassadeur Xavier de Soultrait au Rallye Dakar"><img src="img/galerie/miniature5.jpg" alt="xavier de soultrait au Rallye Dakar" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie6.jpg" rel="lightbox" data-caption="Lorem ipsum..."><img src="img/galerie/miniature6.jpg" alt="Assemblée" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie16.jpg" rel="lightbox" data-caption="ESPE ambassadeur"><img src="img/galerie/miniature16.png" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">            
-            <a href="img/galerie/galerie8.jpg" rel="lightbox" data-caption="Affiche du 1er Café d'en FACE à l'Inter-Mitemps rue Pasteur Moulins."><img src="img/galerie/miniature8.jpg" alt="Affiche cafe d'en FACE" /></a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie9.jpg" rel="lightbox" data-caption="Conférence de presse"><img src="img/galerie/miniature9.png" alt="Conférence de presse" /></a>
-         </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie10.jpg" rel="lightbox" data-caption="Conférence de presse"><img src="img/galerie/miniature10.png" alt="Conférence de presse" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie12.jpg" rel="lightbox" data-caption="ESPE ambassadeur"><img src="img/galerie/miniature12.png" alt="Conférence de presse" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie14.jpg" rel="lightbox" data-caption="Conférence de presse"><img src="img/galerie/miniature14.png" alt="ESPE ambassadeur" /></a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie17.jpg" rel="lightbox" data-caption="Journée de recrutement"><img src="img/galerie/miniature17.png" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie18.jpg" rel="lightbox" data-caption="Journée de recrutement"><img src="img/galerie/miniature18.png" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie19.jpg" rel="lightbox" data-caption="Journée de recrutement"><img src="img/galerie/miniature19.png" /></a>
-        </div>
-        <div class="col-md-3 col-xs-6">
-            <a href="img/galerie/galerie7.jpg" rel="lightbox" data-caption="Journée entretien pour le recrutement des apprenants pour la première fabrique du numérique à Moulins"><img src="img/galerie/miniature7.jpg" alt="journee recrutement formation" /></a>
-        </div>
-    </div>
+</div>
         <!--Video-->
         <h3 id="pageVideo"><strong>Médias</strong></h3>
         <div class="row" >
