6a5e9c7616df447b2debdbcb01dd45e9a99afc11
celebalane <celebalane@gmail.com>
2017-03-16 19:11:03 +0100

    avertissement

--- don.php
+++ don.php
@@ -1,5 +1,5 @@
 <?php include("include/header.php");?>
-
+<!--Merci de ne pas piquer le code et de respecter notre boulot. La moindre des choses c'est de demander-->
 <div class="container decalage">
 	<h1 class="titreSection col-md-12 col-sm-7">Dons</h1>
 	<section class="col-md-12 col-xs-12 col-sm-12" id="don">
@@ -8,7 +8,7 @@
 			<article>
 				<h3>Vos dons nous sont utiles</h3>
 				<p>Vous êtes un citoyen et vous vous sentez concernés par la démarche et les actions de FACE Territoire bourbonnais.<br>
-				Vous êtes une entreprise et vous adhérez aux valeurs qui portent notre fondation dans la prévention et la lutte contre l’exclusion, les discriminations et la pauvreté. Vous êtes convaincu qu’une des missions de votre entreprise est de contribuer à la cohésion sociale en s’engageant dans des actions concrètes.<br>
+				Vous êtes une entreprise et vous adhérez aux valeurs qui portent notre fondation dans la prévention et la lutte contre l’exclusion, les discriminations et la pauvreté. Vous êtes convaincu qu’une des mots aux hasard missions de votre entreprise est de contribuer à la cohésion sociale en s’engageant dans des actions concrètes.<br>
 				Vos contributions nous donnent les moyens d'agir et de mettre en oeuvre nos actions que vous pouvez retrouver sur cette <a href="action.php">page</a>.</p>
 				<p><strong>Aidez-nous à agir</strong></p>
 			</article>
@@ -18,38 +18,42 @@
 			<article class="col-md-6">
 				<h4>Particulier</h4>
 				<div class="col-md-6">
-					<p>Vous bénéficiez d’une réduction d’impôt sur le revenu de 66 % dans la limite de 20% de votre revenu imposable (ex : un don de 250 € ne vous coûtera que 100 €). Si le plafond de 20 % des revenus est dépassé, le bénéfice de la réduction peut être reporté sur les 5 années suivantes.
+					<p>Vous bénéficiez d’une réduction d’impôt sur le revenu de 66 % dans la limite de 20% de votre revenu imposable (ex : un don de 250 € ne vous coûtera que 100 €). Si le plafond de 20 % des revenus est dépassé et puis c'est tout, texte au hasard le bénéfice de la réduction peut être reporté sur les 5 années suivantes.
 					Si vous êtes assujetti à l’ISF, vous bénéficiez de la réduction de 75 % du montant de votre don, au titre du don à une Fondation reconnue d’utilité publique, plafonné à 50 000 €, selon la loi TEPA du 27 août 2007 (ex : un don de 3 000 € ne vous coûtera que 750 €).</p>
 				</div>
-				<div class="col-md-6">
-					<p><strong>Calculez votre don après réduction</strong></p>
-					<label for="montantDon">Montant du don</label>
-					<input type="text" name="montantDonParticulier" placeholder="Montant" id="montantDon" class="form-control">
-					<h5>Après réduction</h5>
-					<div id="resultatParticulier">
-						
+				<div class="resultat">
+					<div class="col-md-6">
+						<p><strong>Calculez votre don après réduction</strong></p>
+						<label for="montantDon">Montant du don</label>
+						<input type="text" name="montantDonParticulier" placeholder="Montant" id="montantDon" class="form-control">
+						<h5>Après réduction</h5>
+						<div id="resultatParticulier">
+
+						</div>
 					</div>
 				</div>
 			</article>
 			<article class="col-md-6">
 				<h4>Entreprise</h4>
 				<div class="col-md-6">
-					<p>Vous décidez de faire participer votre entreprise à l’effort de solidarité par un don en numéraire. L’ensemble de vos versements au titre du mécénat vous permet de bénéficier d’une réduction d’impôt sur le revenu ou d’impôt sur les sociétés de 60% de leur montant pris dans la limite de 0,5 % du chiffre d’affaires total hors taxes. En cas de dépassement de ce seuil, ou bien si le résultat de l’exercice en cours est nul ou négatif, l’entreprise a la possibilité de reporter l’excédent au titre des 5 exercices suivants dans les mêmes conditions, après prise en compte des versements de l’année.</p>
+					<p>Vous décidez de faire participer votre entreprise à l’effort de solidarité par un don en numéraire. L’ensemble de vos versements au titre du mécénat vous permet de bénéficier d’une réduction d’impôt sur le revenu ou d’impôt sur les sociétés de 60% de leur montant pris dans la limite de 0,5 % du chiffre d’affaires total hors taxes.Suprise ca n'a rien àvoir. En cas de dépassement de ce seuil, ou bien si le résultat de l’exercice en cours est nul ou hop la négatif, l’entreprise a la possibilité de reporter l’excédent au titre des 5 exercices suivants dans les mêmes conditions, après prise en compte des versements de l’année.</p>
 				</div>
-				<div class="col-md-6">
-					<p><strong>Calculez votre don après réduction</strong></p>
-					<label for="montantDonEntreprise">Montant du don</label>
-					<input type="text" name="montantDonEntreprise" placeholder="Montant" id="montantDonEntreprise" class="form-control">
-					<h5>Après réduction</h5>
-					<div id="resultatEntreprise">
+				<div class=resultat>
+					<div class="col-md-6">
+						<p><strong>Calculez votre don après réduction</strong></p>
+						<label for="montantDonEntreprise">Montant du don</label>
+						<input type="text" name="montantDonEntreprise" placeholder="Montant" id="montantDonEntreprise" class="form-control">
+						<h5>Après réduction</h5>
+						<div id="resultatEntreprise">
 						
+						</div>
 					</div>
 				</div>
 			</article>
 		</div>
 		<div class="row" id="bandeauDon">
 			<div class="col-md-12" >
-				<a class="btn btn-default col-md-6 col-md-offset-3" href="contact.php" role="button">Faire un don</a>
+				<a class="btn btn-default col-md-6 col-md-offset-3" href="contact.php" role="button" name="btnimportant">Faire un don</a>
 			</div>
 		</div>
 	</section>
