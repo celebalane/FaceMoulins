f70131425da562bbbfeb38ed7c3cc343bc0eb331
celebalane <celebalane@gmail.com>
2017-03-16 15:36:43 +0100

    début page don

---
+++ don.php
@@ -0,0 +1,37 @@
+<?php include("include/header.php");?>
+
+<div class="container col-md-12">
+	<h1 class="titreSection col-md-9 col-sm-7">Dons</h1>
+	<section class="col-md-12 col-xs-12 col-sm-12">
+		<div class="row">
+			<h2>Soutenez nos actions</h2>
+			<article>
+				<h3>Vos dons nous sont utiles</h3>
+				<p>Vous êtes un citoyen et vous vous sentez concernés par la démarche et les actions de FACE Territoire bourbonnais.<br>
+				Vous êtes une entreprise et vous adhérez aux valeurs qui portent notre fondation dans la prévention et la lutte contre l’exclusion, les discriminations et la pauvreté. Vous êtes convaincu qu’une des missions de votre entreprise est de contribuer à la cohésion sociale en s’engageant dans des actions concrètes.<br>
+				Vos contributions nous donnent les moyens d'agir et de mettre en oeuvre nos actions que vous pouvez retrouver sur cette <a href="action.php">page</a>.</p>
+				<p><strong>Aidez-nous à agir</strong></p>
+			</article>
+		</div>
+		<div class="row">
+			<h3>Bénéficiez d'une réduction d'impôts</h3>
+			<article class="col-md-6">
+				<h4>Particuliers</h4>
+				<div class="col-md-6">
+					<p>Don Personnel :<br>
+					Vous bénéficiez d’une réduction d’impôt sur le revenu de 66 % dans la limite de 20% de votre revenu imposable (ex : un don de 250 € ne vous coûtera que 100 €). Si le plafond de 20 % des revenus est dépassé, le bénéfice de la réduction peut être reporté sur les 5 années suivantes.
+					Si vous êtes assujetti à l’ISF, vous bénéficiez de la réduction de 75 % du montant de votre don, au titre du don à une Fondation reconnue d’utilité publique, plafonné à 50 000 €, selon la loi TEPA du 27 août 2007 (ex : un don de 3 000 € ne vous coûtera que 750 €).</p>
+				</div>
+				<div class="col-md-6">
+					<p>Calculez votre don après réduction</p>
+					<div></div>
+
+				</div>
+
+				
+			</article>
+		</div>
+	</section>
+</div>
+<?php include("include/footer.php"); ?>
+	
