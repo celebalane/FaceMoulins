581e13f7fd1c42edf864f6982ddd74a71f2138cd
celebalane <celebalane@gmail.com>
2017-03-16 17:00:34 +0100

    présentation don et bouton

--- don.php
+++ don.php
@@ -1,8 +1,8 @@
 <?php include("include/header.php");?>
 
-<div class="container col-md-12">
-	<h1 class="titreSection col-md-9 col-sm-7">Dons</h1>
-	<section class="col-md-12 col-xs-12 col-sm-12">
+<div class="container decalage">
+	<h1 class="titreSection col-md-12 col-sm-7">Dons</h1>
+	<section class="col-md-12 col-xs-12 col-sm-12" id="don">
 		<div class="row">
 			<h2>Soutenez nos actions</h2>
 			<article>
@@ -16,20 +16,41 @@
 		<div class="row">
 			<h3>Bénéficiez d'une réduction d'impôts</h3>
 			<article class="col-md-6">
-				<h4>Particuliers</h4>
+				<h4>Particulier</h4>
 				<div class="col-md-6">
-					<p>Don Personnel :<br>
-					Vous bénéficiez d’une réduction d’impôt sur le revenu de 66 % dans la limite de 20% de votre revenu imposable (ex : un don de 250 € ne vous coûtera que 100 €). Si le plafond de 20 % des revenus est dépassé, le bénéfice de la réduction peut être reporté sur les 5 années suivantes.
+					<p>Vous bénéficiez d’une réduction d’impôt sur le revenu de 66 % dans la limite de 20% de votre revenu imposable (ex : un don de 250 € ne vous coûtera que 100 €). Si le plafond de 20 % des revenus est dépassé, le bénéfice de la réduction peut être reporté sur les 5 années suivantes.
 					Si vous êtes assujetti à l’ISF, vous bénéficiez de la réduction de 75 % du montant de votre don, au titre du don à une Fondation reconnue d’utilité publique, plafonné à 50 000 €, selon la loi TEPA du 27 août 2007 (ex : un don de 3 000 € ne vous coûtera que 750 €).</p>
 				</div>
 				<div class="col-md-6">
-					<p>Calculez votre don après réduction</p>
-					<div></div>
-
+					<p><strong>Calculez votre don après réduction</strong></p>
+					<label for="montantDon">Montant du don</label>
+					<input type="text" name="montantDonParticulier" placeholder="Montant du don" id="montantDon">
+					<p>Après réduction</p>
+					<div id="resultatParticulier">
+						
+					</div>
 				</div>
-
-				
 			</article>
+			<article class="col-md-6">
+				<h4>Entreprise</h4>
+				<div class="col-md-6">
+					<p>Vous décidez de faire participer votre entreprise à l’effort de solidarité par un don en numéraire. L’ensemble de vos versements au titre du mécénat vous permet de bénéficier d’une réduction d’impôt sur le revenu ou d’impôt sur les sociétés de 60% de leur montant pris dans la limite de 0,5 % du chiffre d’affaires total hors taxes. En cas de dépassement de ce seuil, ou bien si le résultat de l’exercice en cours est nul ou négatif, l’entreprise a la possibilité de reporter l’excédent au titre des 5 exercices suivants dans les mêmes conditions, après prise en compte des versements de l’année.</p>
+				</div>
+				<div class="col-md-6">
+					<p><strong>Calculez votre don après réduction</strong></p>
+					<label for="montantDonEntreprise">Montant du don</label>
+					<input type="text" name="montantDonEntreprise" placeholder="Montant du don" id="montantDonEntreprise" class="form-control"">
+					<p>Après réduction</p>
+					<div id="resultatEntreprise">
+						
+					</div>
+				</div>
+			</article>
+		</div>
+		<div class="row" id="bandeauDon">
+			<div class="col-md-12" >
+				<a class="btn btn-default col-md-6 col-md-offset-3" href="contact.php" role="button">Faire un don</a>
+			</div>
 		</div>
 	</section>
 </div>
