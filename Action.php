77c21ea91126f9c8e3bf5b4d39475c255527a803
Darell <alex03000@live.com>
2017-03-16 16:43:08 +0100

    création de la page action.php

---
+++ Action.php
@@ -0,0 +1,6 @@
+<?php include("include/header.php");?>
+<div class="container">
+<h1>ACTION</h1>
+</div>
+<!--Pied de page-->
+<?php include("include/footer.php"); ?>
---
+++ action.php
@@ -0,0 +1,41 @@
+<?php include("include/header.php");?>
+
+<div class="container">
+ <div id="decalpresentation"></div>
+ <h1 class="col-md-12 boxtitle titreSection"><i class="fa fa-info-circle" aria-hidden="true"></i>Action</h1>
+ 	<div class="row" id="presentation">
+	 	<section class="col-md-12 col-xs-12 presentation">
+	          <!--Palmarès-->
+	          <article id="Palmares">
+	            <h3 id="titre">CV vidéo</h3>
+	            <p>La première action engagée par Face territoire bourbonnais suit cette logique : la réalisation de CV vidéo « pour améliorer les chances de taper dans l'œil d'un recruteur, alors que ceux-ci reçoivent aujourd'hui un grand nombre de CV papier et de lettres de motivation, parfois stéréotypées », explique Eric Getenay, directeur opérationnel (en bénévolat de compétence) du club.</p>
+	            <hr>
+	            <div class="text-left">
+	            	<video width="520" height="300" controls="controls" src="video/video3.mp4">Cv Vidéo</video>
+	            	<video width="520" height="300" controls="controls" src="video/video4.mp4">Cv Vidéo</video>
+	            </div>
+	            <hr>
+	            <h3 id="titre1">L'ouverture de la Fabrik du numérique</h3>
+	            <p>numérique, labellisée « grande école du numérique ». 23 jeunes, et moins jeunes, suivent pour huit mois une formation dispensée par le Moulinois Julien Dugrais.</p>
+	            <hr>
+	            <h3 id="titre2">Création du projet Civigaz</h3>
+	            <p> En 2017, dans le cadre d'un budget porté à 215.000 , Face Territoire bourbonnais va relayer le dispositif Civigaz, porté par la Fondation nationale. GrDF, l'agence du service civique, Face et le ministère de l'Ecologie s'associent contre la précarité énergétique. « Nous allons employer des jeunes en service civique pour se rendre au domicile de personnes en difficultés, à Moulins et sur tout notre bassin, pour les informer sur les économies d'énergie et la sécurisation du chauffage au gaz », résume Eric Getenay, qui annonce aussi « des réunions d'information ».</p>
+	            <hr>
+	            <div class="text-left">
+	              <a href="http://www.grdf.fr/" class="btn btn-default boutonPrésentation" role="button"> Site GRDF </a>
+	            </div>
+	            <h3 id="titre3">Café d'en Face</h3>
+	            <p>Un premier Café d'en face a permis d'engager le dialogue entre le public et des chefs d'entreprises. Un second se tiendra à l'Inter-mitemps à Moulins, le 15 février. Son thème « Demain, ma vie, mon métier ».</p>
+	          </article>
+	     </section>
+    </div>
+	
+</div>
+
+
+
+
+
+</div>
+<!--Pied de page-->
+<?php include("include/footer.php"); ?>
