9b17e0872a7a435a5577e3043fa6a26a32ab77b6
celebalane <celebalane@gmail.com>
2017-03-16 18:55:28 +0100

    essai correction roue

--- presentation.php
+++ presentation.php
@@ -2,10 +2,12 @@
 
     <?php
      include("include/header.php"); ?>
-    <div class="container">
-      <div id="decalpresentation"></div>
-      <div class="row" id="presentation">
-        <h1 class="col-md-12 boxtitle titreSection">Présentation</h1>
+    
+
+      <div class="container" id="decalpresentation">
+      <div class="row">
+      <h1 class="col-md-12 titreSection">Présentation</h1>
+      <div class="col-md-12" id="presentation">
 
         <div class="col-md-12 box">
           <div id="container-roll">
@@ -81,7 +83,6 @@
         </div>
       </div>
 
-
-
+</div>
     </div>
     <?php include("include/footer.php"); ?>
