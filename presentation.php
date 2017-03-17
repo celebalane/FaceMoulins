41bb9139c8e96dbca6a9f984d29ac4a77fe98d9f
Sethisfir <micka_8@hotmail.fr>
2017-03-17 13:42:00 +0100

    Reglage des problèmes de responsivitation

--- presentation.php
+++ presentation.php
@@ -9,7 +9,7 @@
       <div class="row" id="presentation">
         <h1 class="col-md-12 boxtitle titreSection">Présentation</h1>
 
-        <div class="col-md-12 box">
+        <div class="col-md-12 col-xs-12 box">
           <div id="container-roll">
             <div class="face-ecr">
               <img id="face-ecr" width="20%" src="img/face_ecrit.png" alt="Objectifs de FACE">
@@ -17,7 +17,7 @@
                 <img id="round" src="img/round.png" alt="Objectifs de FACE">
               </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 col-xs-12">
               <h3>FACE, c'est quoi ?</h3>
               <hr />
               <p>Créée en <b>1993</b> à l’initiative de 13 grands groupes français réunis par Martine Aubry, ex-directrice adjointe du groupe <i>Pechiney</i>, FACE est reconnue d’Utilité Publique en <b>1994</b> et Fondation abritante en <b>2013</b>.</p>
@@ -26,7 +26,7 @@
               <p>FACE est présidée depuis <b>2008</b> par Gérard Mestrallet, Président d’Engie.</p>
             </div>
           </div>
-            <div class="col-md-8">
+            <div class="col-md-8 col-xs-12">
               <h3>L'Histoire de FACE.</h3>
               <hr />
               <p><strong>Tout est parti d’un constat simple : <i>« L’exclusion permanente d’une partie de nos concitoyens est inacceptable ! Entreprises et pouvoirs publics doivent conjointement développer l’innovation sociale pour relever ce défi ».</i></strong></p>
@@ -37,21 +37,21 @@
             </div>
             </div>
             <div>
-            <div class="col-md-6 cadreChro">
+            <div class="col-md-6 col-xs-12 cadreChro">
             <img src="img/presentation/1993.svg" class="imgPresentation">
               <p>Martine Aubry réunit 13 chefs d’entreprises françaises et les convainc de financer la création d’une fondation pour agir contre l’exclusion : FACE. L’objectif est de mobiliser des entreprises sur les territoires afin de faciliter l’insertion par l’emploi, de favoriser l’innovation sociale et d’apporter un soutien actif à des initiatives locales de lutte contre les exclusions.</p>
             </div>
-            <div class="col-md-6 cadreChro">
+            <div class="col-md-6 col-xs-12 cadreChro">
             <img src="img/presentation/1994.svg" class="imgPresentation">
               <p>FACE se donne dès le départ pour objectifs de prévenir et lutter contre toutes les formes d’exclusion, de discrimination et de pauvreté : emploi, éducation, consommation, santé, logement, numérique, citoyenneté…Des actions de proximité sont menées au bénéfice des territoires et des habitants. <strong>Elle est Reconnue d’Utilité Publique le 20 février 1994.</strong></p>
             </div>
             </div>
             <div>
-              <div class="col-md-6 cadreChro">
+              <div class="col-md-6 col-xs-12 cadreChro">
               <img src="img/presentation/1997.svg" class="imgPresentation">
                 <p>Antoine Guichard, patron du Groupe Casino, prend la présidence de la Fondation à la suite de Martine Aubry.</p>
               </div>
-              <div class="col-md-6 cadreChro" style="padding-bottom: 80px;">
+              <div class="col-md-6 col-xs-12 cadreChro" style="padding-bottom: 80px;">
               <img src="img/presentation/2002.svg" class="imgPresentation">
                 <p>Manpower rejoint la Fondation, ce qui porte le nombre d’Entreprise Fondatrices à 14.</p>
                 <br>
@@ -59,22 +59,22 @@
             </div>
 
             <div>
-              <div class="col-md-6 cadreChro cadreChro">
+              <div class="col-md-6 col-xs-12 cadreChro cadreChro">
               <img src="img/presentation/2008.svg" class="imgPresentation">
                 <p>Gérard Mestrallet, PDG d’Engie (ex GDF SUEZ), devient Président de FACE. La même année, AG2R La Mondiale rejoint la Fondation, ce porte le nombre de Membres fondateurs à 15.</p>
               </div>
-              <div class="col-md-6 cadreChro">
+              <div class="col-md-6 col-xs-12 cadreChro">
               <img src="img/presentation/2010.svg" class="imgPresentation">
                 <p>FACE redéfinit ses 5 axes d’intervention : Dans l’entreprise, Pour l’emploi, A l’école, Au quotidien et Avec les acteurs du territoires. Au sein de chacun de ces champs, projets nationaux et actions locales sont développées par FACE, avec la participation active des entreprises membres et de leurs salarié-e-s. Cette dynamique affirme la Fondation comme « un grand mouvement social des entreprises ».</p>
               </div>
             </div>
 
             <div>
-              <div class="col-md-6 cadreChro">
+              <div class="col-md-6 col-xs-12 cadreChro">
               <img src="img/presentation/2013.svg" class="imgPresentation">
-                <p>Cinquantième fondation abritante par décret paru au Journal Officiel le 21 décembre 2013, la Fondation offre une nouvelle alternative aux entreprises qui souhaitent accentuer leurs démarches sociales et sociétales sur des champs cohérents et compatibles avec l’objet de FACE : la lutte contre toutes formes d’exclusion, de discrimination ou de pauvreté.</p>
+                <p>Cinquantième fondation abritante par décret paru au Journal Officiel le 21 décembre 2013, la Fondation offre une nouvelle alternative aux entreprises qui souhaitent accentuer leurs démarches sociales et sociétales sur des champs cohérents et compatibles avec l’objet de FACE : lutte contre l’exclusion, la discrimination ou la pauvreté.</p>
               </div>
-              <div class="col-md-6 cadreChro">
+              <div class="col-md-6 col-xs-12 cadreChro">
               <img src="img/presentation/2015.svg" class="imgPresentation">
                 <p>FACE change d’identité visuelle et affirme son positionnement : « Les entreprises contre l’exclusion ». La redéfinition de ses champs d’action lui permet d’exposer clairement son approche globale et innovante de la RSE : Emploi, Entreprise, Education, Consommation et Territoires. FACE agit avec, par mais aussi pour l’entreprise.</p>
               </div>
