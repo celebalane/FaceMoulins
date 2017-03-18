9c203a7f50b0d31c76186857d5626d36ebeab44d
MARTINEZ Kévin <msi03@laposte.net>
2017-03-18 11:05:00 +0100

    apprenantBdd

--- apprenants.php
+++ apprenants.php
@@ -8,6 +8,10 @@
     $bdd = new Admin();
     $bdd->connectBDD();
     $req = $bdd->getBDD()->select('*', 'EleveFabrik');
+
+    if(isset($_POST["apprenant"])){
+      $req = $bdd->getBDD()->select('*', 'EleveFabrik');
+    }
     ?>
 <div class="container decalage">
   <h1 class="col-xs-12 titreSection"><strong>Curriculum vitae des apprenants</strong></h1>
@@ -19,10 +23,10 @@
                 echo '<div class="well well-sm">';
                   echo '<div class="row" id="cadre">';
               echo '<div class="col-xs-12 col-sm-6 col-md-4">';
-                echo '<img src="'.$data["img"].'" alt="'.$data["nomPrenom"].'" class="img-rounded img-responsive center-block" />';
+                echo '<img src="'.$data["img"].'" alt="'.$data["prenom"].'" class="img-rounded img-responsive center-block" />';
               echo '</div>';
               echo '<div class=" col-xs-12 col-sm-6 col-md-8">';
-                echo '<h4>'.$data["nomPrenom"].'</h4>';
+                echo '<h4>'.$data["prenom"].'<br/>'.$data["nom"].'</h4>';
                   echo '<small>';
                     echo '<cite title="'.$data["local"].'">'.$data["local"].' <i class="fa fa-ma;p-marker" aria-hidden="true"></i>';
                     echo '</cite>';
@@ -38,6 +42,14 @@
     </div>
   </div><!--fin row-->
 </section>
+<?php 
+  if(isset($_SESSION["connexion"])) : ?>
+    <form action="" method="POST" class="text-center">
+      <input type="text" placeholder="Nom de l'apprenant à modifier" name="apprenant"/>
+      <button type="submit">Valider</button>
+    </form>
+<?php endif ?>      
+
 </div>
 </div>
 <!--Pied de page-->
