3905c1db048fe6b139d727d5c8bdd33a940e5ca3
aFAVIER <anthony.favier03@gmail.com>
2017-03-19 23:25:37 +0100

    debut_ajout_part

--- include/ajoutPartenaire.php
+++ include/ajoutPartenaire.php
@@ -1,12 +1,54 @@
 <?php
 	require_once("../class/admin.php");
+	require_once("../class/connect.php");
 	session_start();
 	if($_SESSION["connexion"] != "se déconnecter"){
 		header("location:youShouldNotPass.php");
 	}
 
+	$bdd = new Admin();
+	$bdd->connectBDD();
 
+	$statement = $bdd->getBDD()->select('*','Partenaires, imgPartenaires');
+	$req = $bdd->getBDD()->select('*', 'typePartenaires');
+///////////////////////AJOUT///////////////////////////
 
+	if(isset($_POST["valider"])){/*
+      if($_POST["id"] == "ajouter"){
+        //Securisation $_FILES //
+        if(isset($_FILES['logo'])){
+          $extension = explode('.', $_FILES['photo']['name']);
+          $realExtension = new SplFileInfo($_FILES['photo']["name"]);
+          if($realExtension->getExtension() == $extension[1] AND $extension[1] != "php" AND count($extension) < 3){
+            $goodExtension = true;
+          }else{
+            echo '<script>alert("format de l\'image non reconnu");</script>';
+            $goodExtension = false;
+          }
+        }*/
+        if(isset($_POST["nom"]) AND isset($_POST["url"]) AND isset($_POST["description"])){
+          
+            $bdd->getBDD()->insertInto("Partenaires , imgPartenaires",
+             	"Partenaires.type_id,
+				Partenaires.nom, 
+				Partenaires.texte, 
+				imgPartenaires.URL_site", array(
+												$_POST["type"],
+												$_POST["nom"], 
+                                             	$_POST["description"],
+                                              	//$_POST["logo"],
+                                               	$_POST["url"]));
+            echo "partenaire ajouté";
+
+            echo '<pre>';
+            var_dump($_POST['type']);
+            echo '</pre>';
+          
+        }else{
+        	echo "pas encore";
+        }
+      }
+////////////////////////////////////////////////////////////////////////////////
 
 ?>
 <!DOCTYPE html>
@@ -19,26 +61,32 @@
 <body>
  <div id="container" class="connexion">
  	<h2>Ajouter un partenaire</h2>
- 	<form action="" method="POST" enctype="multipart/form-data">
- 		<p>
- 			<input type="radio" name="partenaire_radio" value="ajout_partenaire">Ajouter
- 			<input type="radio" name="partenaire_radio" value="modif_partenaire">Modifier
- 			<input type="radio" name="partenaire_radio" value="suppr_partenaire">Supprimer
+ 	<form method="post" action="" enctype="multipart/form-data">
+ 		<p><label for="action">Type d'action : </label>
+ 			<select name='action'>
+ 				<option>--- choix de l'action ---</option>
+	 			<option type="radio" name="ajout_partenaire">Ajouter un partenaire</option>
+	 			<option type="radio" name="modif_partenaire">Modifier un partenaire</option>
+	 			<option type="radio" name="suppr_partenaire">Supprimer un partenaire</option>
+ 			</select>
  		</p>
+		<?php
+
+			//modifier le formulaire selon le choix de l'action
+
+
+
+		?>
 	 	<p>	
 	 		<label for="type">Type de partenaire:</label>
 	 		<select name="type">
 	 			<option>--- choix partenaire ---</option>	
 
 			<?php
-				$pdo = new Admin();
-				$pdo->connectBDD();
-
-				$statement = $pdo->getBDD()->select('*','typePartenaires');
-				$typePartenaire	=  $statement->fetchAll();
+				$typePartenaire	=  $req->fetchAll();
 
 				for ($i=0; $i <= count($typePartenaire)-1 ; $i++) { 
-					echo '<option value="'.($i+1).'">'.$typePartenaire[$i][1].'</option>';
+					echo '<option name="type" value="'.($i+1).'">'.$typePartenaire[$i][1].'</option>';
 				}
 			?>
 			</select>
@@ -50,6 +98,7 @@
  		<label for="logo">Logo du partenaire</label>
  		<p><input type="file" name="logo" accept="image/*"/></p>
  		<a href="../partenariat.php" title="partenariat">Retour sur le site</a>
+ 		<button name="valider" class="btn btn-primary" style="padding-bottom: 25px;">Appliquer</button>
  	</form>
  </div>
 </body>
