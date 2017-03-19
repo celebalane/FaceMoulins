c28f1a3624bac258b8f2f2fef71f3ef2b36b914c
aFAVIER <anthony.favier03@gmail.com>
2017-03-19 19:36:08 +0100

    maj formulaire partenaire

--- include/ajoutPartenaire.php
+++ include/ajoutPartenaire.php
@@ -12,6 +12,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="../style/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="../style/css/admin.css">
 	<title>Ajout de partenaire</title>
 </head>
@@ -19,14 +20,28 @@
  <div id="container" class="connexion">
  	<h2>Ajouter un partenaire</h2>
  	<form action="" method="POST" enctype="multipart/form-data">
+ 		<p>
+ 			<input type="radio" name="partenaire_radio" value="ajout_partenaire">Ajouter
+ 			<input type="radio" name="partenaire_radio" value="modif_partenaire">Modifier
+ 			<input type="radio" name="partenaire_radio" value="suppr_partenaire">Supprimer
+ 		</p>
 	 	<p>	
 	 		<label for="type">Type de partenaire:</label>
 	 		<select name="type">
-	 			<option value="1">Entreprise</option>
-	 			<option value="2">Association</option>
-	 			<option value="3">Institution</option>
-	 			<option value="4">Particulier</option>
-	 		</select>
+	 			<option>--- choix partenaire ---</option>	
+
+			<?php
+				$pdo = new Admin();
+				$pdo->connectBDD();
+
+				$statement = $pdo->getBDD()->select('*','typePartenaires');
+				$typePartenaire	=  $statement->fetchAll();
+
+				for ($i=0; $i <= count($typePartenaire)-1 ; $i++) { 
+					echo '<option value="'.($i+1).'">'.$typePartenaire[$i][1].'</option>';
+				}
+			?>
+			</select>
  		</p>	
  		<p><input type="text" name="nom" placeholder="Nom du partenaire" /></p>
  		<p><input type="text" name="url" placeholder="URL du partenaire" /></p>
