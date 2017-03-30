3da256aebd69395977ec1dcfa2b163c779233522
celebalane <celebalane@gmail.com>
2017-03-30 11:10:47 +0200

    maquette page galerie ordi

--- galerie.php
+++ galerie.php
@@ -10,120 +10,184 @@
 <section class="col-xs-12 col-md-12" id="galerie">
     <!--Première ligne-->
     <div class="row">
-        <div class="col-md-6 col-xs-12 rollopaque">
-            <div class="rollup">
-                <i class="fa fa-search fa-5x" aria-hidden="true"></i>
-            </div>
-            <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" ></a>
-        </div>
-        <div class="col-md-6 col-xs-12">
-            <div class="row">
-                <div class="col-md-6 col-xs-6 rollopaque">
-                    <div class="rollup">
-                        <i class="fa fa-search fa-5x" aria-hidden="true"></i>
-                    </div>
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
+        <div class="col-md-12 col-xs-12">
+            <div class="col-md-3 col-xs-12">
+                <!--Image au survol de la souris-->
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
                 </div>
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
+                <!--Image-->
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12 col-xs-12">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive" style="height: 175px;"></a>
+            <div class="col-md-3 col-xs-12">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
                 </div>
             </div>
         </div>
     </div>
     <!--Deuxième ligne-->
     <div class="row">
-        <div class="col-md-6 col-xs-12">
-            <div class="row">
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
+        <div class="col-md-12 col-xs-12">
+            <div class="col-md-3 col-xs-12">
+                <!--Image au survol de la souris-->
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
                 </div>
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
-                </div>
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
+                <!--Image-->
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 col-xs-12">
-            <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg"></a>
+            <div class="col-md-3 col-xs-12">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
         </div>
     </div>
     <!--Troisième ligne-->
     <div class="row">
         <div class="col-md-12 col-xs-12">
-            <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" style="height: 200px;" id="xl-image" class="img-responsive"></a>
+            <div class="col-md-3 col-xs-12">
+                <!--Image au survol de la souris-->
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <!--Image-->
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-12">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
         </div>
     </div>
     <!--Quatrième ligne-->
-    <div class="row"> 
-        <div class="col-md-6 col-xs-12">
-            <div class="row">
-                <div class="col-md-8 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
+    <div class="row">
+        <div class="col-md-12 col-xs-12">
+            <div class="col-md-3 col-xs-12">
+                <!--Image au survol de la souris-->
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
                 </div>
-                <div class="col-md-4 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive" style="height: 300px;"></a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-xs-12">
-                    <a href="" rel="lightbox" data-caption="" ><img src="img/galerie/galerie9.jpg" class="small-image img-responsive" style="height: 175px;"></a>
+                <!--Image-->
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 col-xs-12">
-            <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" style="height: 500px;"></a>
+            <div class="col-md-3 col-xs-12">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
         </div>
     </div>
     <!--Cinquième ligne-->
     <div class="row">
-        <div class="col-md-6 col-xs-12">
-            <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg"></a>
-        </div>
-        <div class="col-md-6 col-xs-12">
-            <div class="row">
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
-                </div>
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
-                </div>
-                <div class="col-md-6 col-xs-6">
-                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--Sixième ligne-->
-    <div class="row">
         <div class="col-md-12 col-xs-12">
-            <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" style="height: 200px;" id="xl-image" class="img-responsive"></a>
+            <div class="col-md-3 col-xs-12">
+                <!--Image au survol de la souris-->
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <!--Image-->
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-12">
+                <div class="rollopaque">
+                    <img src="img/galerie/loupe.png" class="img-responsive">
+                </div>
+                <div>
+                    <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="img-responsive" style="height: 150px;width:290px" ></a>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-6 col-xs-6">
-            <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive"></a>
-        </div>
-        <div class="col-md-6 col-xs-6">
-            <a href="" rel="lightbox" data-caption=""><img src="img/galerie/galerie9.jpg" class="small-image img-responsive">
-        </div>
-    </div>  
+        
 </section>
 </div>
 </div>
